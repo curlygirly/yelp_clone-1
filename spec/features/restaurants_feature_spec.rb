2b00196732d1ab62ae5cf036b8665630bc9fdfde
Jennifer Arad-Marks <jlaradmarks@gmail.com>
2015-06-17 13:17:48 +0100

    Only signed in Users can make changes- all tests passing

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -7,6 +7,7 @@ feature 'restaurants' do
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
+      sign_in
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
     end
